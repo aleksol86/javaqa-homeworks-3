ac6a732b5bb8520c59dfba33cd114e62f5d843c4
Aleksandr Olenin <aleksandr.olenin@mail.ru>
2020-02-25 13:11:36 +0300

    Удалён Main1.java

--- Main1.java
+++
@@ -1,8 +0,0 @@
-public class Main1 {
-    public static void main(String[] args) {
-        int balance = 2_000_000_000;
-        int transfer = 500_000_000;
-        int total = balance - transfer;
-        System.out.println(total);
-    }
-}
